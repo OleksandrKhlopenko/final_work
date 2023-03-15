89a9bbbc944c54d18bb9e7f028421f22c4634221
OleksandrKhlopenko <108742474+OleksandrKhlopenko@users.noreply.github.com>
2023-03-15 14:21:28 +0200

    deleted  wishCard2 component, rename wishlist and Button

---
+++ src/components/Wishlist/Buttons/Button.jsx
@@ -0,0 +1,9 @@
+import  styles from "../../../pages/Wishlist/Wishlist.module.scss"
+
+const Button = () => {
+  return (
+    <button className={styles.btnBtnDark}>Add To Cart</button>
+  )
+}
+
+export default Button
